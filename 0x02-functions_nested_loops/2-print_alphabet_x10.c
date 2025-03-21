a22d1267bfc55b4cebf5055d56420dd2810d9058
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-03-21 18:32:37 +0000

    this the thrid work in the three taks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/* this program is to print the alphabet _10 times */
+/* this is the _3 of day three work*/
+int main()
+{
+int i;
+char alphabet;
+for(i = 0; i < 10; i++);
+{
+	for (alphabet = 'a'; alphabet <= 'z';);
+		_putchar(alphabet);
+			_putchar("\n");
+			return 0;
+}
+
+}
